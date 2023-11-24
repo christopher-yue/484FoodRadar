d634948195afb5970860ff2a378721cb05cd04b8
MikePik18 <michaelpikounis18@gmail.com>
2023-11-24 14:32:36 -0500

    added index page

--- frontend/src/components/navbar/loginNavbar.js
+++ frontend/src/components/navbar/loginNavbar.js
@@ -1,20 +1,22 @@
 import { Link } from "react-router-dom";
-import "./loginNavbar.css";
+import "./navbar.css";
 
 export const LoginNavbar = () => {
   return (
-    <header>
-      <ul className="navmenu">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
-      </ul>
-    </header>
+    <div className="navbar">
+      <header>
+        <ul className="navmenu">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+        </ul>
+      </header>
+    </div>
   );
 };
--- frontend/src/views/index/indexView.js
+++ frontend/src/views/index/indexView.js
@@ -1,11 +1,51 @@
 import React from "react";
 import { LoginNavbar } from "../../components/navbar/loginNavbar";
+import { Footer } from "../../components/footer/footer";
+import "./indexView.css";
+import Filter from "../../images/filter.svg";
+import Pin from "../../images/pin.svg";
+import Calendar from "../../images/calendar.svg";
 
 export const IndexView = () => {
   return (
-    <div>
+    <div className="page">
       <LoginNavbar />
-      <h1>Index</h1>
+      <div className="head">
+        <div class="logo-container">
+            <img src="logo.png" alt="FoodRadar Logo" class="logo" />
+            <h1>FoodRadar</h1>
+        </div>
+        <p>Discover the best food options in your area!</p>
+      </div>
+
+      <div className="main">
+        <section class="hero">
+          <h2>Discover Local Delights</h2>
+          <p>Explore a world of culinary experiences right in your neighborhood. From cozy cafes to gourmet restaurants, FoodRadar helps you find the perfect dining spot.</p>
+          <a href="/signup" class="cta-button">Get Started</a>
+        </section>
+
+        <section class="features">
+          <div class="feature">
+            <img src={Filter} alt="Filter Icon" />
+            <h3>Smart Filters</h3>
+            <p>Refine your search with smart filters such as cuisine, price range, and ratings to discover the best options tailored to your preferences.</p>
+          </div>
+
+          <div class="feature">
+            <img src={Pin} alt="Location Icon" />
+            <h3>Location-Based Search</h3>
+            <p>Find hidden gems nearby using our location-based search. Uncover new flavors and experiences just around the corner.</p>
+          </div>
+
+          <div class="feature">
+            <img src={Calendar} alt="Calendar Icon" />
+            <h3>Table Booking</h3>
+            <p>Secure your spot at your favorite restaurants. With FoodRadar, you can easily make and manage reservations for a hassle-free dining experience.</p>
+          </div>
+        </section>
+      </div>
+      <Footer />
     </div>
   );
 };
