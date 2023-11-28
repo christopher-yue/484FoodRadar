bbcf969d373f96b990dd5974693e60c95823dc9d
MikePik18 <michaelpikounis18@gmail.com>
2023-11-28 14:58:46 -0500

    added date/time to reservation

--- frontend/src/views/place/placeDetails.js
+++ frontend/src/views/place/placeDetails.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import { Navbar } from '../../components/navbar/navbar';
+import { Footer } from '../../components/footer/footer';
 import { useLocation, useNavigate } from 'react-router-dom';
 import './placeDetails.css';
 
@@ -14,13 +16,32 @@ export const PlaceDetails = () => {
 
     return (
         <div className="place-details-container">
+            <Navbar />
             <h1>{place.name}</h1>
             <div className="photos-container">
                 {place.photoUrls && place.photoUrls.map((url, index) => (
                     <img key={index} src={url} alt={place.name} />
                 ))}
             </div>
+            <div>
+                <input
+                className='date'
+                type='date'
+                placeholder='Date'
+                />
+                <input
+                className='time'
+                type='time'
+                placeholder='Time'
+                />
+                <input
+                className='numPeople'
+                type='text'
+                placeholder='Number of People'
+                />
+            </div>
             <button className="add-reservation-btn">Add to Reservations</button>
+            <Footer />
         </div>
     );
 };
