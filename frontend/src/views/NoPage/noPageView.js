2a8816cc18f66cc9cc34e067ccc48b39782975a6
Nicolas Fontes de Mello <nicolasfontesdemello@hotmail.com>
2023-11-29 17:19:28 -0500

    Added a 404 error opage to frontend/src/views/NoPage/noPageView.js

---
+++ frontend/src/views/NoPage/noPageView.js
@@ -0,0 +1,12 @@
+// frontend/src/views/NoPage/noPageView.js
+import React, { useContext } from "react";
+
+export default function noPageView() {
+  return(
+    <>
+    
+    <React />
+    <h2>ERROR 404: NOT FOUND</h2>    
+    </>
+  )
+}
