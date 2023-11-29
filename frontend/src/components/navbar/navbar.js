da5685b6244b6612b5195094c01d390cc6ad6e86
MikePik18 <michaelpikounis18@gmail.com>
2023-11-28 19:34:38 -0500

    navbar

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -39,13 +39,14 @@ export const Navbar = () => {
                     {isLoggedIn ? (
                         // Menu for logged in users
                         <>
-                            <li><Link to="/profile" onClick={toggleMenu}>Profile</Link></li>
-                            <li><Link to="/managereservations" onClick={toggleMenu}>Manage Reservations</Link></li>
+                            <li><Link to="/home" onClick={toggleMenu}>Home</Link></li>
+                            <li><Link to="/managereservation" onClick={toggleMenu}>Profile/Manage Reservations</Link></li>
                             <li><LogoutButton onClick={handleLogout}>Log Out</LogoutButton></li>
                         </>
                     ) : (
                         // Menu for guests (not logged in)
                         <>
+                            <li><Link to="/" onClick={toggleMenu}>Home</Link></li>
                             <li><Link to="/signup" onClick={toggleMenu}>Sign Up</Link></li>
                             <li><Link to="/login" onClick={toggleMenu}>Login</Link></li>
                         </>
